b27a0463c84b8416033c6c68b80e7318dc0886f7
jenny <jenny88062@gmail.com>
2022-05-05 11:36:14 +0800

    upload two

--- guess_num.py
+++ guess_num.py
@@ -7,13 +7,16 @@ Created on Fri Apr 29 07:14:44 2022
 
 import random
 
-r = random.randint(1, 100)
+start_num = int(input('請輸入猜數字範圍的起始值: '))
+end_num = int(input('請輸入猜數字範圍的末端值: '))
+
+r = random.randint(start_num, end_num)
 #print(r)
 count = 0
 
 while True:
     count = count +1 
-    guess_num = int(input('請猜1~100的數字: '))
+    guess_num = int(input('請猜數字: '))
     if guess_num == r :
         print('您猜對號碼了!')
         print('這是您猜的第', count ,'次')
